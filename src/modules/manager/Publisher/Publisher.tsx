5259057aa08f8d94dc169173143744ee232de9af
JBJ <maystar5987@gmail.com>
2023-11-07 18:20:45 +0900

    ..

--- src/modules/manager/Publisher/Publisher.tsx
+++ src/modules/manager/Publisher/Publisher.tsx
@@ -4,15 +4,26 @@ import { Link } from "react-router-dom";
 
 function PublisherTable() {
   const [publishers, setPublishers] = useState([]);
+  const [newPublishers, setNewPublishers] = useState([]); // 추가된 부분
 
   useEffect(() => {
     axios
-      .get("/api/publishers") // 백엔드에서 제공하는 API 주소를 입력하세요.
+      .get("/api/publishers")
       .then((response) => {
         setPublishers(response.data);
       })
       .catch((error) => {
         console.error("Error fetching data: ", error);
+      });
+
+    // 새로운 'publisher' 정보를 받아와 'newPublishers' 상태에 저장합니다.
+    axios
+      .get("/api/new-publishers")
+      .then((response) => {
+        setNewPublishers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching new publishers: ", error);
       });
   }, []);
 
@@ -22,6 +33,7 @@ function PublisherTable() {
         <tr>
           <th>Publisher</th>
           <th>Number of Books</th>
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
@@ -31,6 +43,14 @@ function PublisherTable() {
               <Link to={`/publishers/${encodeURIComponent(publisher.name)}`}>{publisher.name}</Link>
             </td>
             <td>{publisher.count}</td>
+            <td>
+              {/* 'newPublishers' 배열에 현재 'publisher'가 있는지 확인하여 'NEW' 버튼을 표시하거나 숨깁니다. */}
+              {newPublishers.includes(publisher.name) && (
+                <Link to={`/newBookByPublisher/${encodeURIComponent(publisher.name)}`}>
+                  <button>NEW</button>
+                </Link>
+              )}
+            </td>
           </tr>
         ))}
       </tbody>
--- src/modules/manager/ShopOrder/order.tsx
+++ src/modules/manager/ShopOrder/order.tsx
@@ -15,30 +15,60 @@ const Order = () => {
     };
   }, []);
 
+  const handleApproval = async (orderId) => {
+    // 주문 접수 완료 메시지를 서버로 보내는 코드
+    // 서버 엔드포인트와 요청 메소드는 서버 설정에 따라 변경해야 합니다.
+    try {
+      const response = await fetch(`http://localhost:8082/store/orders/${orderId}/approve`, {
+        method: "POST",
+      });
+
+      if (!response.ok) {
+        throw new Error("Order approval failed");
+      }
+
+      alert("Order approved successfully");
+    } catch (error) {
+      console.error("Failed to approve order: ", error);
+    }
+  };
+
   return (
     <div>
       <h1>Orders</h1>
-      {orders.map(
-        (
-          order,
-          index, // 주문 정보를 반복하여 출력
-        ) => (
-          <div key={index}>
-            <h2>Order #{order.id}</h2>
-            <p>Name: {order.name}</p>
-            <p>Address: {order.address}</p>
-            <h3>Items</h3>
-            {order.orderSalesItems.map((item, i) => (
-              <div key={i}>
-                <p>Product ID: {item.productId}</p>
-                <p>Product Name: {item.productName}</p>
-                <p>Quantity: {item.quantity}</p>
-                <p>Unit Price: {item.unitPrice}</p>
-              </div>
-            ))}
-          </div>
-        ),
-      )}
+      <table>
+        <thead>
+          <tr>
+            <th>Order ID</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Items</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order, index) => (
+            <tr key={index}>
+              <td>{order.id}</td>
+              <td>{order.name}</td>
+              <td>{order.address}</td>
+              <td>
+                {order.orderSalesItems.map((item, i) => (
+                  <div key={i}>
+                    <p>Product ID: {item.productId}</p>
+                    <p>Product Name: {item.productName}</p>
+                    <p>Quantity: {item.quantity}</p>
+                    <p>Unit Price: {item.unitPrice}</p>
+                  </div>
+                ))}
+              </td>
+              <td>
+                <button onClick={() => handleApproval(order.id)}>Approve</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
