a41dbda30dddbfb4e0dcf5a67d9d8d8ee60b8dc1
kim05412 <rlathgus541@naver.com>
2023-11-30 22:08:29 +0900

    webpack 성능 최적화

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ProvidePlugin } = require("webpack");
+const env = require("process");
+// 빌드하는 것의 용량 보기
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+// performance Guide
 
+// 자바스크립에서 타입스크립트 처럼 타입 맞춰서 쓰고 싶을때 사용
 /** @type {import('webpack').Configuration} */
 module.exports = {
   entry: "./src/index.tsx",
@@ -38,10 +43,24 @@ module.exports = {
     new ProvidePlugin({
       React: "react",
     }),
+
+    // 번들 분석기
+    !env.WEBPACK_SERVE
+      ? new BundleAnalyzerPlugin({
+          analyzerMode: "static",
+          openAnalyzer: false,
+        })
+      : null,
   ],
   devServer: {
     historyApiFallback: true,
     static: "./dist",
     open: true,
   },
+  // 빌드 캐시 최적화
+  cache: {
+    type: env.WEBPACK_SERVE ? "memory" : "filesystem",
+  },
+  // 소스맵 최적화
+  devtool: env.WEBPACK_SERVE ? "eval-cheap-module-source-map" : false,
 };
